22acecc333b78add2bf098487fedd74760374543
Nyakaru <moses.kinara@andela.com>
2019-07-17 12:03:55 +0300

    feat (orders): orders endpoints and validations
    - mongoose validation
    - managing orders
    - populating queries
    - uploading image
    
    [Finishes #]

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,43 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
-const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg')
+    cb(null, true);
+   else {
+  cb(null, false)
+  }
+};
+
+const upload = multer({storage: storage, limits: {
+  fileSize: 1024 * 1024 * 5 },
+  imageFilter: imageFilter
+});
 
 import Product from '../models/product'
 
-router.get('/', (req, res, next) => {
+const router = express.Router();
+
+router.get('/products', (req, res, next) => {
     Product.find()
+      .select('name price _id productImage')
       .exec()
       .then(docs => {
-          console.log(docs);
-          res.status(200).json(docs);
+          const responseBody = {
+              count: docs.length,
+              products: docs
+          };
+          res.status(200).json(responseBody);
       })
       .catch(err => {
           console.log(err);
@@ -18,18 +45,25 @@ router.get('/', (req, res, next) => {
       });
   });
 
-router.post('/', (req, res, next) => {
+router.post('/products', upload.single('productImage'), (req, res, next) => {
+  console.log(req.file);
   const product = new Product({
       _id: new mongoose.Types.ObjectId(),
       name: req.body.name,
-      price: req.body.price
+      price: req.body.price,
+      productImage: req.file.path
   });
   product.save()
   .then(result => {
       console.log(result);
       res.status(201).json({
         message: 'Product created successfully',
-        createdProduct: result
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            productImage: result.productImage
+        }
     });
   })
   .catch(err => {
@@ -38,16 +72,17 @@ router.post('/', (req, res, next) => {
 });
 });
 
-router.get('/:productId', (req, res, next) => {
+router.get('/products/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
         console.log(doc);
         if (doc) {
           res.status(200).json(doc);
         } else {
-          res.status(404).json({message: 'No valid entry found for ID provided'});
+          res.status(404).json({message: 'Product not found'});
         }
         
     })
@@ -57,17 +92,18 @@ router.get('/:productId', (req, res, next) => {
     });
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/products/:productId', (req, res, next) => {
   const id = req.params.productId;
   const updateOps = {};
   for (const ops of req.body) {
       updateOps[ops.propName] =ops.value;
   }
-  Product.update({_id: id}, {$set: updateOps})
+  Product.updateOne({_id: id}, {$set: updateOps})
     .exec()
     .then(result => {
-        console.log(result);
-        res.status(200).json(result);
+        res.status(200).json({
+          message: 'Product updated successfully'
+        });
     })
     .catch(err => {
       console.log(err);
@@ -75,12 +111,14 @@ router.patch('/:productId', (req, res, next) => {
   });
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/products/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.remove({_id: id})
       .exec()
       .then(result => {
-          res.status(200).json(result);
+          res.status(200).json({
+            message: 'Product deleted successfully'
+          });
       })
       .catch(err => {
         console.log(err);
